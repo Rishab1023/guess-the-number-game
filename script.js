461fe5eb428fdbb643f287e553dcabc47abd18c8
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-04-14 15:44:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 let GameNO = 25;
 
 let UserNO = prompt("Enter the number")
+
+// while(GameNO != UserNO){
+//     UserNO = prompt("You are wrong. Guess again");
+// }
+
+// console.log("Congrats you have guessed the correct number");
